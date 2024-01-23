0bdfe668330921097b920912d182888ceff567cf
Logan Dhillon <logandhi64@gmail.com>
2024-01-23 17:03:35 -0500

    Optimize imports

--- src/main/java/frc/robot/exception/ConfigMotorException.java
+++ src/main/java/frc/robot/exception/ConfigMotorException.java
@@ -1,7 +1,5 @@
 package frc.robot.exception;
 
-import com.revrobotics.CANSparkMax;
-
 import edu.wpi.first.wpilibj.motorcontrol.MotorController;
 
 public class ConfigMotorException extends RuntimeException {
