afa597c9d7827a548a11d7a241be784b88748bf5
gummy-bear1 <90622825+gummy-bear1@users.noreply.github.com>
2024-03-02 11:47:45 -0500

    merge conflict

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -67,10 +67,10 @@ public class RobotContainer {
         // Commands
         private final Command cmd_teleopDrive;
 
-        private final SwerveRequest.FieldCentric teleopDrive = new SwerveRequest.FieldCentric()
-                        .withDeadband(kDrive.kMaxDriveVelocity * 0.1)
-                        .withRotationalDeadband(kDrive.kMaxTurnAngularVelocity * 0.1)
-                        .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
+        // private final SwerveRequest.FieldCentric teleopDrive = new SwerveRequest.FieldCentric()
+        //                 .withDeadband(kDrive.kMaxDriveVelocity * 0.1)
+        //                 .withRotationalDeadband(kDrive.kMaxTurnAngularVelocity * 0.1)
+        //                 .withDriveRequestType(DriveRequestType.OpenLoopVoltage);
 
         // Shuffleboard
         public final ShuffleboardTab sb_driveteamTab;
