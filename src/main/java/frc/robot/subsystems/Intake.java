20ecf3314fa7784ee4851882cca04b4dd440f2e8
Yonina W <100385243+yoninaW4917@users.noreply.github.com>
2024-03-02 14:02:03 -0500

    uncommented sensor code

--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -4,6 +4,7 @@ import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
 import com.revrobotics.CANSparkMax;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 
@@ -22,10 +23,10 @@ public class Intake extends SubsystemBase {
 	private final CANSparkMax motor;
 
 	// Sensors
-	// private final DigitalInput irSensor;
+	private final DigitalInput irSensor;
 
 	// Shuffleboard
-	// private final ShuffleboardTab sb_tab;
+	private final ShuffleboardTab sb_tab;
 
 	private Intake() {
 		// Motors
@@ -34,11 +35,11 @@ public class Intake extends SubsystemBase {
 		configMotor(motor, false);
 
 		// Laser sensor
-		// irSensor = new DigitalInput(0);
+		irSensor = new DigitalInput(0);
 
 		// Shuffleboard
-		// sb_tab = Shuffleboard.getTab("Intake");
-		// sb_tab.addBoolean("IR Sensor Value", () -> checkIR());
+		sb_tab = Shuffleboard.getTab("Intake");
+		sb_tab.addBoolean("IR Sensor Value", () -> checkIR());
 	}
 
 	// Get subsystem
@@ -62,9 +63,9 @@ public class Intake extends SubsystemBase {
 	 * 
 	 * @return True if laser is interrupted.
 	 */
-	// public boolean checkIR() {
-	// return !irSensor.get();
-	// }
+	public boolean checkIR() {
+		return !irSensor.get();
+	}
 
 	/**
 	 * Set voltage of motor.
