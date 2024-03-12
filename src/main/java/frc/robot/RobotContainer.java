6eff7502074cab8364c1e64fc0b5540246ac494f
gummy-bear1 <90622825+gummy-bear1@users.noreply.github.com>
2024-03-12 14:24:34 -0400

    re format

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -49,213 +49,213 @@ import frc.robot.subsystems.Intake;
  */
 public class RobotContainer {
 
-        /**
-         * The container for the robot. Contains subsystems, OI devices, and commands.
-         */
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    // Joysticks
+    private final CommandXboxController m_primaryController;
+    private final CommandXboxController m_secondaryController;
+
+    // Subsystems
+    public final Drivetrain sys_drivetrain;
+    public final Climber sys_climber;
+    public final Intake sys_intake;
+    public final Indexer sys_indexer;
+    private final Deployment sys_deployment;
+    public final Cartridge sys_cartridge;
+
+    // Commands
+    private final Command cmd_teleopDrive;
+
+    // Shuffleboard
+    public final ShuffleboardTab sb_driveteamTab;
+
+    // Autonomous
+    private final SendableChooser<Command> sc_autoChooser;
+
+    /**
+     * The container for the robot. Contains subsystems, OI devices, and commands.
+     */
+    public RobotContainer() {
+
         // Joysticks
-        private final CommandXboxController m_primaryController;
-        private final CommandXboxController m_secondaryController;
+        m_primaryController = new CommandXboxController(kControllers.PRIMARY_CONTROLLER);
+        m_secondaryController = new CommandXboxController(kControllers.SECONDARY_CONTROLLER);
+        DriverStation.silenceJoystickConnectionWarning(true);
 
         // Subsystems
-        public final Drivetrain sys_drivetrain;
-        public final Climber sys_climber;
-        public final Intake sys_intake;
-        public final Indexer sys_indexer;
-        private final Deployment sys_deployment;
-        public final Cartridge sys_cartridge;
+        sys_climber = new Climber();
+        sys_intake = Intake.getInstance();
+        sys_indexer = Indexer.getInstance();
+        sys_deployment = new Deployment();
+        sys_cartridge = Cartridge.getInstance();
+        // Subsystems
+        sys_drivetrain = kRobot.IS_BETA_ROBOT
+                ? TunerConstantsBeta.DriveTrain
+                : TunerConstantsComp.DriveTrain;
 
         // Commands
-        private final Command cmd_teleopDrive;
+        cmd_teleopDrive = sys_drivetrain.drive(
+                () -> -m_primaryController.getLeftY() * kDrive.MAX_DRIVE_VELOCIY,
+                () -> -m_primaryController.getLeftX() * kDrive.MAX_DRIVE_VELOCIY,
+                () -> (m_primaryController.getLeftTriggerAxis()
+                        - m_primaryController.getRightTriggerAxis())
+                        * kDrive.MAX_TURN_ANGULAR_VELOCITY);
+
+        sys_drivetrain.setDefaultCommand(cmd_teleopDrive);
+
+        registerPathplannerCommands();
 
         // Shuffleboard
-        public final ShuffleboardTab sb_driveteamTab;
+        sb_driveteamTab = Shuffleboard.getTab("Drive team");
+        sc_autoChooser = AutoBuilder.buildAutoChooser();
+        addShuffleboardItems();
+
+        // Configure the trigger bindings
+        configureBindings();
+    }
+
+    /**
+     * Use this method to define your trigger->command mappings. Triggers can be
+     * created via the
+     * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+     * an arbitrary
+     * predicate, or via the named factories in {@link
+     * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+     * {@link
+     * CommandXboxController
+     * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+     * PS4} controllers or
+     * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+     * joysticks}.
+     */
+    private void configureBindings() {
+
+        // Primary Controller
+
+        // drivetrain
+        m_primaryController.rightBumper()
+                .onTrue(Commands.runOnce(sys_drivetrain::seedFieldRelative, sys_drivetrain));
+
+        // score command
+        m_primaryController.a()
+                .onTrue(new ScoreNote(sys_deployment, sys_cartridge));
+
+        // Intake note command
+        m_primaryController.x()
+                .whileTrue(Commands.race(
+                        Commands.startEnd(
+                                () -> {
+                                    sys_intake.setVoltage(kIntake.VOLTAGE);
+                                    sys_indexer.setVoltage(kIndexer.VOLTAGE);
+                                },
+                                () -> {
+                                    sys_intake.setVoltage(0);
+                                    sys_indexer.setVoltage(0);
+                                },
+                                sys_intake, sys_indexer),
+                        Commands.waitUntil(() -> sys_indexer.checkIR())));
+
+        // Eject note command
+        m_primaryController.b()
+                .whileTrue(Commands.startEnd(
+                        () -> {
+                            sys_intake.setVoltage(-kIntake.VOLTAGE);
+                            sys_indexer.setVoltage(-kIndexer.VOLTAGE);
+                        },
+                        () -> {
+                            sys_intake.setVoltage(0);
+                            sys_indexer.setVoltage(0);
+                        },
+                        sys_intake, sys_indexer));
+
+        // Secondary Controller
+        // *************************************************************************************************************
+
+        // Manual climber movement up
+        m_secondaryController.povUp()
+                .onTrue(Commands.runOnce(() -> sys_climber.setVoltage(-Constants.kClimber.VOLTAGE),
+                        sys_climber))
+                .onFalse(Commands.runOnce(() -> sys_climber.setVoltage(0), sys_climber));
+
+        // Manual climber movement down
+        m_secondaryController.povDown()
+                .onTrue(Commands.runOnce(() -> sys_climber.setVoltage(Constants.kClimber.VOLTAGE),
+                        sys_climber))
+                .onFalse(Commands.runOnce(() -> sys_climber.setVoltage(0), sys_climber));
+
+        // climber setpoint high
+        m_secondaryController.y()
+                .onTrue(Commands.runOnce(
+                        () -> sys_climber.setPosition(Constants.kClimber.HIGH,
+                                Constants.kClimber.KFAST_SLOT),
+                        sys_climber));
+
+        // climber setpoint low
+        m_secondaryController.x()
+                .onTrue(Commands.runOnce(
+                        () -> sys_climber.setPosition(Constants.kClimber.HIGH,
+                                Constants.kClimber.KLOW_SLOT),
+                        sys_climber));
+
+        // climber setpoint low
+        m_secondaryController.a()
+                .onTrue(Commands.runOnce(
+                        () -> sys_climber.setPosition(Constants.kClimber.LOW,
+                                Constants.kClimber.KFAST_SLOT),
+                        sys_climber));
+
+        // Bring note to cartridge
+        m_secondaryController.b()
+                .onTrue(new BringNoteToCartridge(sys_cartridge, sys_indexer));
+
+        // Climb, extend and score, endgame sequence
+        m_secondaryController.start()
+                .onTrue(new ScoreTrap(sys_deployment, sys_cartridge, sys_climber));
+    }
+
+    private void addShuffleboardItems() {
+
+        // Re-zero
+        sb_driveteamTab.add("Seed field relative",
+                Commands.runOnce(sys_drivetrain::seedFieldRelative, sys_drivetrain))
+                .withPosition(0, 0);
 
         // Autonomous
-        private final SendableChooser<Command> sc_autoChooser;
+        sb_driveteamTab.add("Choose auto", sc_autoChooser)
+                .withPosition(0, 1)
+                .withSize(3, 1);
 
-        /**
-         * The container for the robot. Contains subsystems, OI devices, and commands.
-         */
-        public RobotContainer() {
+    }
 
-                // Joysticks
-                m_primaryController = new CommandXboxController(kControllers.PRIMARY_CONTROLLER);
-                m_secondaryController = new CommandXboxController(kControllers.SECONDARY_CONTROLLER);
-                DriverStation.silenceJoystickConnectionWarning(true);
+    public void registerPathplannerCommands() {
 
-                // Subsystems
-                sys_climber = new Climber();
-                sys_intake = Intake.getInstance();
-                sys_indexer = Indexer.getInstance();
-                sys_deployment = new Deployment();
-                sys_cartridge = Cartridge.getInstance();
-                // Subsystems
-                sys_drivetrain = kRobot.IS_BETA_ROBOT
-                                ? TunerConstantsBeta.DriveTrain
-                                : TunerConstantsComp.DriveTrain;
+        NamedCommands.registerCommand("IntakeFromFloor",
+                Commands.race(
+                        Commands.startEnd(
+                                () -> {
+                                    sys_intake.setVoltage(kIntake.VOLTAGE);
+                                    sys_indexer.setVoltage(kIndexer.VOLTAGE);
+                                },
+                                () -> {
+                                    sys_intake.setVoltage(0);
+                                    sys_indexer.setVoltage(0);
+                                },
+                                sys_intake, sys_indexer),
+                        Commands.waitUntil(() -> sys_indexer.checkIR())));
 
-                // Commands
-                cmd_teleopDrive = sys_drivetrain.drive(
-                                () -> -m_primaryController.getLeftY() * kDrive.MAX_DRIVE_VELOCIY,
-                                () -> -m_primaryController.getLeftX() * kDrive.MAX_DRIVE_VELOCIY,
-                                () -> (m_primaryController.getLeftTriggerAxis()
-                                                - m_primaryController.getRightTriggerAxis())
-                                                * kDrive.MAX_TURN_ANGULAR_VELOCITY);
+        NamedCommands.registerCommand("BringNoteToCartridge",
+                new BringNoteToCartridge(sys_cartridge, sys_indexer));
+        NamedCommands.registerCommand("ScoreNote", new ScoreNote(sys_deployment, sys_cartridge).withTimeout(2));
 
-                sys_drivetrain.setDefaultCommand(cmd_teleopDrive);
+    }
 
-                registerPathplannerCommands();
-
-                // Shuffleboard
-                sb_driveteamTab = Shuffleboard.getTab("Drive team");
-                sc_autoChooser = AutoBuilder.buildAutoChooser();
-                addShuffleboardItems();
-
-                // Configure the trigger bindings
-                configureBindings();
-        }
-
-        /**
-         * Use this method to define your trigger->command mappings. Triggers can be
-         * created via the
-         * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
-         * an arbitrary
-         * predicate, or via the named factories in {@link
-         * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
-         * {@link
-         * CommandXboxController
-         * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-         * PS4} controllers or
-         * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-         * joysticks}.
-         */
-        private void configureBindings() {
-
-                // Primary Controller
-
-                // drivetrain
-                m_primaryController.rightBumper()
-                                .onTrue(Commands.runOnce(sys_drivetrain::seedFieldRelative, sys_drivetrain));
-
-                // score command
-                m_primaryController.a()
-                                .onTrue(new ScoreNote(sys_deployment, sys_cartridge));
-
-                // Intake note command
-                m_primaryController.x()
-                                .whileTrue(Commands.race(
-                                                Commands.startEnd(
-                                                                () -> {
-                                                                        sys_intake.setVoltage(kIntake.VOLTAGE);
-                                                                        sys_indexer.setVoltage(kIndexer.VOLTAGE);
-                                                                },
-                                                                () -> {
-                                                                        sys_intake.setVoltage(0);
-                                                                        sys_indexer.setVoltage(0);
-                                                                },
-                                                                sys_intake, sys_indexer),
-                                                Commands.waitUntil(() -> sys_indexer.checkIR())));
-
-                // Eject note command
-                m_primaryController.b()
-                                .whileTrue(Commands.startEnd(
-                                                () -> {
-                                                        sys_intake.setVoltage(-kIntake.VOLTAGE);
-                                                        sys_indexer.setVoltage(-kIndexer.VOLTAGE);
-                                                },
-                                                () -> {
-                                                        sys_intake.setVoltage(0);
-                                                        sys_indexer.setVoltage(0);
-                                                },
-                                                sys_intake, sys_indexer));
-
-                // Secondary Controller
-                // *************************************************************************************************************
-
-                // Manual climber movement up
-                m_secondaryController.povUp()
-                                .onTrue(Commands.runOnce(() -> sys_climber.setVoltage(-Constants.kClimber.VOLTAGE),
-                                                sys_climber))
-                                .onFalse(Commands.runOnce(() -> sys_climber.setVoltage(0), sys_climber));
-
-                // Manual climber movement down
-                m_secondaryController.povDown()
-                                .onTrue(Commands.runOnce(() -> sys_climber.setVoltage(Constants.kClimber.VOLTAGE),
-                                                sys_climber))
-                                .onFalse(Commands.runOnce(() -> sys_climber.setVoltage(0), sys_climber));
-
-                // climber setpoint high
-                m_secondaryController.y()
-                                .onTrue(Commands.runOnce(
-                                                () -> sys_climber.setPosition(Constants.kClimber.HIGH,
-                                                                Constants.kClimber.KFAST_SLOT),
-                                                sys_climber));
-
-                // climber setpoint low
-                m_secondaryController.x()
-                                .onTrue(Commands.runOnce(
-                                                () -> sys_climber.setPosition(Constants.kClimber.HIGH,
-                                                                Constants.kClimber.KLOW_SLOT),
-                                                sys_climber));
-
-                // climber setpoint low
-                m_secondaryController.a()
-                                .onTrue(Commands.runOnce(
-                                                () -> sys_climber.setPosition(Constants.kClimber.LOW,
-                                                                Constants.kClimber.KFAST_SLOT),
-                                                sys_climber));
-
-                // Bring note to cartridge
-                m_secondaryController.b()
-                                .onTrue(new BringNoteToCartridge(sys_cartridge, sys_indexer));
-
-                // Climb, extend and score, endgame sequence
-                m_secondaryController.start()
-                                .onTrue(new ScoreTrap(sys_deployment, sys_cartridge, sys_climber));
-        }
-
-        private void addShuffleboardItems() {
-
-                // Re-zero
-                sb_driveteamTab.add("Seed field relative",
-                                Commands.runOnce(sys_drivetrain::seedFieldRelative, sys_drivetrain))
-                                .withPosition(0, 0);
-
-                // Autonomous
-                sb_driveteamTab.add("Choose auto", sc_autoChooser)
-                                .withPosition(0, 1)
-                                .withSize(3, 1);
-
-        }
-
-        public void registerPathplannerCommands() {
-
-                NamedCommands.registerCommand("IntakeFromFloor",
-                                Commands.race(
-                                                Commands.startEnd(
-                                                                () -> {
-                                                                        sys_intake.setVoltage(kIntake.VOLTAGE);
-                                                                        sys_indexer.setVoltage(kIndexer.VOLTAGE);
-                                                                },
-                                                                () -> {
-                                                                        sys_intake.setVoltage(0);
-                                                                        sys_indexer.setVoltage(0);
-                                                                },
-                                                                sys_intake, sys_indexer),
-                                                Commands.waitUntil(() -> sys_indexer.checkIR())));
-
-                NamedCommands.registerCommand("BringNoteToCartridge",
-                                new BringNoteToCartridge(sys_cartridge, sys_indexer));
-                NamedCommands.registerCommand("ScoreNote", new ScoreNote(sys_deployment, sys_cartridge).withTimeout(2));
-
-        }
-
-        /**
-         * Use this to pass the autonomous command to the main {@link Robot} class.
-         *
-         * @return the command to run in autonomous
-         */
-        public Command getAutonomousCommand() {
-                return sc_autoChooser.getSelected();
-        }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    public Command getAutonomousCommand() {
+        return sc_autoChooser.getSelected();
+    }
 }
