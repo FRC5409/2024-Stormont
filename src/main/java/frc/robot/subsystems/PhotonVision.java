a84d1f7796cd111d834262fdd478436eb1e42ede
Slynyr <99285452+Slynyr@users.noreply.github.com>
2024-03-13 12:09:43 -0400

    Removed depricated method
    - Removed depricated method from PhotonVision subsystem

--- src/main/java/frc/robot/subsystems/PhotonVision.java
+++ src/main/java/frc/robot/subsystems/PhotonVision.java
@@ -60,43 +60,8 @@ public class PhotonVision extends SubsystemBase {
    * Returns Optional containing positioning data retrieved through april tags. If
    * there are no april tags visible, an empty optional will be returned.
    * 
-   * @param prevEstimatedPose Lsat estimated position //TODO depricate
    * @return Positioning data
    */
-  public Optional<EstimatedRobotPose> getEstimatedGlobalPose(Pose2d prevEstimatedPose) {
-    poseEstimatorFront.setReferencePose(prevEstimatedPose);
-    poseEstimatorBack.setReferencePose(prevEstimatedPose);
-
-    if (backCamera.isConnected()) {
-      Optional<EstimatedRobotPose> photonDataFront = poseEstimatorFront.update();
-      Optional<EstimatedRobotPose> photonDataBack = poseEstimatorBack.update();
-      Optional<EstimatedRobotPose> photonDataOut;
-
-      if (photonDataFront.isPresent() || photonDataBack.isPresent()) {
-
-        if (photonDataFront.isPresent() && photonDataBack.isPresent()) {
-          if (getMeasurementAmbiguity(
-              photonDataFront.get().targetsUsed) < (getMeasurementAmbiguity(photonDataBack.get().targetsUsed))) {
-            photonDataOut = photonDataFront;
-          } else {
-            photonDataOut = photonDataBack;
-          }
-        } else if (photonDataFront.isPresent()) {
-          photonDataOut = photonDataFront;
-        } else {
-          photonDataOut = photonDataBack;
-        }
-
-        return isWithinAmbiguityThreshold(photonDataOut.get().targetsUsed, kPhotonVision.AMBIGUITY_THRESHOLD)
-            ? photonDataOut
-            : Optional.empty();
-      }
-    } else {
-      // System.out.println("CAMERA NOT CONNECTED");
-    }
-    return Optional.empty();
-  }
-
   public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
     Optional<EstimatedRobotPose> poseEstimateFront = getPoseEstimatorUpdate(frontCamera, poseEstimatorFront);
     Optional<EstimatedRobotPose> poseEstimateBack = getPoseEstimatorUpdate(backCamera, poseEstimatorBack);
