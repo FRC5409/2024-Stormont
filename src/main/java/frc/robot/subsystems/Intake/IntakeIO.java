98c92559e160b26d39f50b2b2d42c11dd879f7ce
reneerenko <1titarenkren@hdsb.ca>
2024-11-12 13:49:22 -0800

    demo logging intake

---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIO.java
@@ -0,0 +1,19 @@
+package frc.robot.subsystems.Intake;
+
+import org.littletonrobotics.junction.AutoLog;
+
+public interface IntakeIO {
+    @AutoLog
+    public class IntakeInputs{
+        public boolean motorConnected = false;
+        public double motorCurrent = 0.0;
+        public double motorVolts = 0.0;
+        public double motorSpeed = 0.0;
+        public double motorTemp = 0.0;
+    }
+
+    public default void setVoltage (double volts){}
+
+    public default void updateInputs (IntakeInputs inputs){}
+    
+}
---
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOSparkMax.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems.Intake;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+
+import edu.wpi.first.wpilibj.RobotController;
+
+import com.revrobotics.CANSparkBase.IdleMode;
+import com.revrobotics.CANSparkLowLevel.MotorType;
+
+public class IntakeIOSparkMax implements IntakeIO {
+
+    private CANSparkMax intakeMotor;
+    private RelativeEncoder intakeIncoder;
+
+    public IntakeIOSparkMax(int ID) {
+        intakeMotor = new CANSparkMax(ID, MotorType.kBrushless);
+        
+        intakeMotor.restoreFactoryDefaults();
+        intakeMotor.setSmartCurrentLimit(30);
+        intakeMotor.setIdleMode(IdleMode.kBrake);
+        intakeMotor.setInverted(false);
+
+        intakeMotor.burnFlash();
+
+        intakeIncoder = intakeMotor.getEncoder();
+    }
+
+
+    @Override
+    public void setVoltage(double volts) {
+        intakeMotor.setVoltage(volts);
+    }
+
+    @Override
+    public void updateInputs (IntakeInputs inputs) {
+        inputs.motorConnected = true;
+        inputs.motorVolts = intakeMotor.get()*RobotController.getBatteryVoltage();
+        inputs.motorCurrent = intakeMotor.getOutputCurrent();
+        inputs.motorTemp = intakeMotor.getMotorTemperature();
+        inputs.motorSpeed = intakeIncoder.getVelocity();
+    }
+
+    
+}
