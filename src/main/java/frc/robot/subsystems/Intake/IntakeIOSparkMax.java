c7c414e33889b11bf5f824f96890f093db1d72df
Logan Dhillon <logandhi64@gmail.com>
2024-11-12 16:59:53 -0500

    Implement IR sensor logging

--- src/main/java/frc/robot/subsystems/Intake/IntakeIOSparkMax.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeIOSparkMax.java
@@ -1,15 +1,21 @@
 package frc.robot.subsystems.intake;
 
+import com.revrobotics.CANSparkBase.FaultID;
 import com.revrobotics.CANSparkBase.IdleMode;
 import com.revrobotics.CANSparkLowLevel.MotorType;
+
+import javax.imageio.spi.ImageReaderWriterSpi;
+
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotController;
 
 public class IntakeIOSparkMax implements IntakeIO {
     private CANSparkMax motor;
     private RelativeEncoder encoder;
+    private DigitalInput irSensor;
     
     public IntakeIOSparkMax(int id) {
         motor = new CANSparkMax(id, MotorType.kBrushless);
@@ -30,10 +36,11 @@ public class IntakeIOSparkMax implements IntakeIO {
 
     @Override
     public void updateInputs(IntakeInputs inputs) {
+        inputs.motorConnected = !(motor.getFault(FaultID.kMotorFault) || motor.getFault(FaultID.kCANRX) || motor.getFault(FaultID.kCANTX));
         inputs.motorVoltage = motor.get() * RobotController.getBatteryVoltage();
         inputs.motorCurrent = motor.getOutputCurrent();
         inputs.motorTemp = motor.getMotorTemperature();
         inputs.motorSpeed = encoder.getVelocity();
-        inputs.motorConnected = true;
+        inputs.irTripped = !irSensor.get();
     }
 }
